32a2169e61e054090e966a4672eb1046e3af0dc9
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-04-16 15:42:36 +0200

    [add] function to generate password - milestone 1

--- index.php
+++ index.php
@@ -1,5 +1,47 @@
 <!-- PHP -->
+<?php
 
+// Dati
+$charactersType = [
+  'letters', 'capitalLetters', 'numbers', 'symbols'
+];
+
+$symbolsList = [
+  '!', '?', '&', '%', '$', '<', '>', '^', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@', '#', '_', '='
+];
+
+$input_length = intval($_GET['inputLength']);
+
+//Condizione messaggio di output
+if (empty($input_length)) {
+  $output_message = 'Nessun parametro valido inserito';
+} else {
+  $output_message = passwordGenerator($input_length, $charactersType, $symbolsList);
+}
+
+// Funzione di generazione password
+function passwordGenerator($input_length, $charactersType, $symbolsList)
+{
+  $password = [];
+  for ($i = 0; $i < $input_length; $i++) {
+    $indexType = mt_rand(0, count($charactersType) - 1);
+
+    if ($charactersType[$indexType] === 'letters') {
+      $character = strtolower(chr(64 + rand(1, 26)));
+    } elseif ($charactersType[$indexType] === 'capitalLetters') {
+      $character = chr(64 + rand(1, 26));
+    } elseif ($charactersType[$indexType] === 'numbers') {
+      $character = mt_rand(0, 9);
+    } elseif ($charactersType[$indexType] === 'symbols') {
+      $character = $symbolsList[mt_rand(0, count($symbolsList) - 1)];
+    }
+
+    $password[] = $character;
+  }
+  return implode('', $password);
+}
+
+?>
 <!-- /PHP -->
 
 <!-- HTML -->
@@ -24,13 +66,13 @@
   <h3 class="text-center my-3">Genera una password sicura</h3>
 
   <div class="container">
-    <div class="output_box my-3">Nessun parametro valido inserito</div>
+    <div class="output_box my-3"><?php echo $output_message ?></div>
 
     <div class="form_box">
       <form action="index.php" method="GET">
         <div class="mb-3 d-flex align-items-center">
           <label for="inputLength" class="me-5">Lunghezza password:</label>
-          <input type="text" class="form-control" id="inputLength" name="inputLength">
+          <input type="number" class="form-control" id="inputLength" name="inputLength">
         </div>
         <button class="btn btn-primary">Invia</button>
       </form>
